254821e0bbf786eb54190ac8fb587d09722d40cb
teetau <58399765+teetau@users.noreply.github.com>
2019-12-01 21:35:44 +0200

    Create 01hello_world.py

---
+++ 01hello_world.py
@@ -0,0 +1,7 @@
+# 01hello_world.py
+# ensimmäinen koodi
+from microbit import *
+# kirjoitetaan vaikka Hello world !
+display.scroll("Hello world!")
+# teksti rullaa näytöllä
+# Huom ! ääkkösiä ei tueta
