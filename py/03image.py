774c70d107eaa4250b83236696ebb7fecb0bbafd
teetau <58399765+teetau@users.noreply.github.com>
2019-12-02 10:56:10 +0200

    Create 03image.py

---
+++ py/03image.py
@@ -0,0 +1,9 @@
+from microbit import *
+# kuvan luonti manuaalisesti
+# tehdään kuva
+# jokainen rivi erikseen ylhäältä alas, vasemmalta oikealle
+# jokainen ledi kerrallaan.
+nuoli = Image("00900:""09990:""77777:""05550:""02220")
+# ledin kirkkautta voi säätää 0:sta 9:siin
+# nolla on sammunut ja ysi täysillä
+display.show(nuoli)
