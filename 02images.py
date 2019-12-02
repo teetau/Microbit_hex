efd788779a07ec97fc06b53b6894da7f04b13b39
teetau <58399765+teetau@users.noreply.github.com>
2019-12-02 10:32:43 +0200

    Add files via upload

---
+++ 02images.py
@@ -0,0 +1,4 @@
+from microbit import *
+# harjoitellaan kuvan hakemista. Tähän tarvittaisiin nyt kirjasto
+# Image.HAPPY - esimerkki kuvasta, kellot on ryhmä, joka animoidaan
+display.show(Image.ALL_CLOCKS, loop=True, delay=100)
