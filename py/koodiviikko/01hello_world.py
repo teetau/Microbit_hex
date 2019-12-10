503fc2abfb9f857f9c881ac275f00cc0cc1826e8
teetau <58399765+teetau@users.noreply.github.com>
2019-12-10 10:06:27 +0200

    Delete 01hello_world.py

--- py/koodiviikko/01hello_world.py
+++
@@ -1,7 +0,0 @@
-# 01hello_world.py
-# tuodaan kirjasto
-from microbit import *
-# kirjoitetaan vaikka Hello world !
-display.scroll("Hello world!")
-# teksti rullaa näytöllä
-# Huom ! ääkkösiä ei tueta
