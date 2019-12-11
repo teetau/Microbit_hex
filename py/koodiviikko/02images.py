8699b6518fa84fc2dde90be475516991114fa6de
teetau <58399765+teetau@users.noreply.github.com>
2019-12-11 20:20:14 +0200

    Delete 02images.py

--- py/koodiviikko/02images.py
+++
@@ -1,4 +0,0 @@
-from microbit import *
-# harjoitellaan kuvan hakemista. Tähän tarvittaisiin nyt kirjasto
-# Image.HAPPY - esimerkki kuvasta, kellot on ryhmä, joka animoidaan
-display.show(Image.ALL_CLOCKS, loop=True, delay=100)
