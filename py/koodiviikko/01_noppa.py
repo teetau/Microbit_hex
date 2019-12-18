cae7bfff1f44d4057f809d799a4951b9535c04df
teetau <tauriat@gmail.com>
2019-12-18 08:16:18 +0200

    update

--- py/koodiviikko/01_noppa.py
+++
@@ -1,8 +0,0 @@
-from microbit import *
-import random
-
-while True:
-    noppa = random.randint(1, 6)
-    asento = accelerometer.current_gesture()
-    if asento == "shake":
-        display.show(noppa)
